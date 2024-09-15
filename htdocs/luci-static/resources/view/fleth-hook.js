4945870cd90b597f74f1b47cb49f334a9b79a83f
huggy <i@huggy.moe>
2024-09-15 18:17:34 +0900

    0.5 add firewall hook {random port, check port available in mape} tab typo

---
+++ htdocs/luci-static/resources/view/fleth-hook.js
@@ -0,0 +1,101 @@
+let mape_status = [];
+const lang_en = [
+  "Random Port",
+  "You can't access this port from MAP-E interface",
+];
+const lang_ja = [
+  "ランダムポート",
+  "このポートにはMAP-Eインターフェースからアクセスできません",
+];
+const lang_pack = document.documentElement.lang === "ja" ? lang_ja : lang_en;
+
+async function FWPFhook() {
+  if (mape_status.length === 0) {
+    mape_status = (
+      (await L.fs.exec("/usr/sbin/fleth", ["mape_status"])).stdout || ""
+    ).split("\n");
+  }
+  let dom_src_interface = document.querySelector('[data-name="src"]');
+  if (!dom_src_interface) {
+    return;
+  }
+  // WIP match mape interface
+  let ports =
+    mape_status.length > 10
+      ? mape_status[mape_status.length - 1].split(" ")
+      : new Array(65535).fill(1).map((_, i) => i + 1);
+  // let dom_src_zone_list = dom_src_interface.querySelectorAll('.dropdown > li');
+  // let dom_src_interface_list = null
+  // let src_interface_list = []
+  // function get_src_interface_list(){
+  //     src_interface_list = []
+  //     dom_src_interface.querySelector('li[selected]').querySelectorAll('.ifacebadge').forEach(d=>{
+  //         src_interface_list.push(d.innerText.split(':')[0])
+  //     })
+  // }
+  // for (let i = 0; i < dom_src_interface_list.length; i++) {
+  //     dom_src_interface_list[i].addEventListener('click',()=>{
+  //         get_src_interface_list();
+  //     })
+  // }
+  // WIP filter used ports
+  const dom_src_dport = document.querySelector('[data-name="src_dport"]');
+  const dom_src_dport_input = dom_src_dport.querySelector("input");
+  const dom_random_button = document.createElement("button");
+  dom_random_button.classList = "cbi-button";
+  dom_random_button.innerText = lang_pack[0];
+  dom_random_button.style.marginTop = ".5rem";
+  dom_random_button.addEventListener("click", () => {
+    dom_src_dport_input.value = ports[Math.floor(Math.random() * ports.length)];
+    dom_src_dport_input.dispatchEvent(new Event("change", { bubbles: true }));
+    dom_src_dport_input.classList.remove("cbi-input-invalid");
+  });
+  const dom_port_invaild_alert = document.createElement("div");
+  dom_port_invaild_alert.classList = "cbi-value-description";
+  dom_port_invaild_alert.style.color = "#F44336";
+  let old_t = 0;
+  dom_src_dport_input.addEventListener("input", () => {
+    const t = +new Date();
+    old_t = t;
+    setTimeout(() => {
+      if (t === old_t) {
+        if (ports.includes(dom_src_dport_input.value)) {
+          dom_port_invaild_alert.innerText = "";
+          dom_src_dport_input.classList.remove("cbi-input-invalid");
+        } else {
+          dom_port_invaild_alert.innerText = lang_pack[1];
+          dom_src_dport_input.classList.add("cbi-input-invalid");
+        }
+      }
+    }, 200);
+  });
+  dom_src_dport
+    .querySelector(".cbi-value-description")
+    .before(dom_port_invaild_alert);
+  dom_src_dport
+    .querySelector(".cbi-value-description")
+    .before(dom_random_button);
+}
+if (location.pathname === "/cgi-bin/luci/admin/network/firewall/forwards") {
+  const observer = new MutationObserver((mutationsList) => {
+    mutationsList.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          const matchingNodes = node.querySelectorAll(
+            'div[data-name="src_dport"]'
+          );
+          if (matchingNodes.length > 0) {
+            FWPFhook();
+          }
+        }
+      });
+    });
+  });
+  observer.observe(document.querySelector("#modal_overlay"), {
+    childList: true,
+    attributes: true,
+    subtree: true,
+    attributeFilter: ["data-name"],
+  });
+  FWPFhook();
+}
